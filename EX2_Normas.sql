303a55ce3f54a13e343f49584db8529c0d7f6b6c
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-06-13 10:37:51 -0300

    Script Atividades Modelagem

--- EX2_Normas.sql
+++ EX2_Normas.sql
@@ -1,4 +1,4 @@
---9. SQL para listar vendas por cliente 
+--9. sql para listar vendas por cliente 
 --(utilizar qualquer cliente para teste)
 select c.nome as nome_cliente, p.nome as nome_produto
 from vendas_produto vp
@@ -7,16 +7,16 @@ inner join cliente c on c.id = v.cliente
 inner join produto p on p.id = vp.produto;
 
 
---8. SQL para listar clientes por cidade 
+--8. sql para listar clientes por cidade 
 --(utilizar qualquer cidade para testes)
 
 select cli.nome 
 from cliente cli
 inner join endereço e on e.id = cli.id 
-inner join cidade c on c.id = e.id and c.nome = 'Tubarão';
+inner join cidade c on c.id = e.id and c.nome = 'tubarão';
 
 
---10. SQL com o tipo de join mais adequado para listar todos 
+--10. sql com o tipo de join mais adequado para listar todos 
 --os produtos e categorias (mesmo que não tenha produto 
 --cadastrado para uma categoria específica).
 
@@ -28,38 +28,38 @@ from categoria_de_produtos cat
 left join produto pro on pro.tipo = cat.id 
 where pro.tipo is null;  
 
---11. Elaborar comandos SQL para mais três relatórios 
+--11. elaborar comandos sql para mais três relatórios 
 --gerenciais (se coloque no lugar do usuário do sistema para 
 --saber quais informações são importantes nesse contexto).
 
  --filtrar as vendas com base na quantidade de produtos vendidos
-SELECT c.nome as nome_cliente, vp.venda as venda_do_cliente
+select c.nome as nome_cliente, vp.venda as venda_do_cliente
 , p.nome as nome_produto, vp.qt as quantidade
-FROM vendas_produto vp
-INNER JOIN vendas v ON v.id = vp.venda
-INNER JOIN cliente c ON c.id = v.cliente
-INNER JOIN produto p ON p.id = vp.produto
-WHERE vp.qt >= 6;
+from vendas_produto vp
+inner join vendas v on v.id = vp.venda
+inner join cliente c on c.id = v.cliente
+inner join produto p on p.id = vp.produto
+where vp.qt >= 6;
 
 
 
 --filtrar por nome do produto
-SELECT c.nome as nome_cliente, vp.venda as venda_do_cliente, p.nome as nome_produto
-FROM vendas_produto vp
-INNER JOIN vendas v ON v.id = vp.venda
-INNER JOIN cliente c ON c.id = v.cliente
-INNER JOIN produto p ON p.id = vp.produto
-WHERE p.nome = 'Miojo';
+select c.nome as nome_cliente, vp.venda as venda_do_cliente, p.nome as nome_produto
+from vendas_produto vp
+inner join vendas v on v.id = vp.venda
+inner join cliente c on c.id = v.cliente
+inner join produto p on p.id = vp.produto
+where p.nome = 'miojo';
 
 
 --filtrar por cliente
-SELECT c.nome as nome_cliente, vp.venda as venda_do_cliente
+select c.nome as nome_cliente, vp.venda as venda_do_cliente
 , p.nome as nome_produto, vp.qt as quantidade
-FROM vendas_produto vp
-INNER JOIN vendas v ON v.id = vp.venda
-INNER JOIN cliente c ON c.id = v.cliente
-INNER JOIN produto p ON p.id = vp.produto
-WHERE c.nome = 'Carla';
+from vendas_produto vp
+inner join vendas v on v.id = vp.venda
+inner join cliente c on c.id = v.cliente
+inner join produto p on p.id = vp.produto
+where c.nome = 'carla';
 
 
 
